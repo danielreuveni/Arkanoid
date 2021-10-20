9730b8b9a9c4e7def4475b8b8cc747cfa10e3572
Daniel Reuveni <73079447+danielreuveni@users.noreply.github.com>
2021-10-20 21:13:20 +0300

    Delete HitListener.java

--- HitListener.java
+++
@@ -1,16 +0,0 @@
-/** * @author <Daniel Reuveni><drdrdrq@gmail.com>
- ID: <207982869>
- * @version 1
- * @since 2020-24-06
- */
-/** this interface define hitlistener objects.
- */
-public interface HitListener {
-
-     /**This method is called whenever the beingHit object is hit.
-     * The hitter parameter is the Ball that's doing the hitting.
-     * @param beingHit the block will be hit
-     * @param hitter the hitter ball
-     */
-    void hitEvent(Block beingHit, Ball hitter);
-}
